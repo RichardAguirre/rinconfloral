4ce10624e150d4abbbaaa49ddb5a1fef0cc5ec75
carlos <carloscano1033@gmail.com>
2023-11-26 20:19:18 -0500

    add:categoria plantas

---
+++ src/pages/Plantas.js
@@ -0,0 +1,21 @@
+import SideHeadline from "../Components/SideHeadline";
+import Article from "../Components/Article";
+import Table from "../img/Tisch.png"
+import Footer from "../Components/Footer";
+
+const Plantas = () => {
+    document.querySelector("body").style.background = "";
+    return ( 
+        <div>
+            <main>
+                <section className="">
+                    <SideHeadline headline="Plantas" categorie="Productos"/>
+                    <Article image={Table} description="Tenemos diferentes tipos de plantas decorativas para que mejores y des vida a tus espacios" />
+                </section>
+            </main>
+            <Footer />
+        </div>
+     );
+}
+ 
+export default Plantas;
